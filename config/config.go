0f56cb2d147127bb3d296ae38c83d8c21d53fc85
nakano_mac <tatsunori.dia@gmail.com>
2022-01-19 09:13:32 +0900

    logging

--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ import (
 type ConfigList struct {
 	ApiKey    string
 	ApiSecret string
+	LogFile   string
 }
 
 var Config ConfigList
@@ -17,12 +18,13 @@ var Config ConfigList
 func init() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		log.Printf("Fail to read file: %v", err)
+		log.Printf("Failed to read file: %v", err)
 		os.Exit(1)
 	}
 
 	Config = ConfigList{
 		ApiKey:    cfg.Section("bitflyer").Key("api_key").String(),
 		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile:   cfg.Section("gotrading").Key("log_file").String(),
 	}
 }
